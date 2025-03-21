c6214ee0c087aa3cf10c5ec17f1a1b89229404c7
Raj Sah <rajsah5557@gmail.com>
2025-03-21 14:58:35 +0530

    landing page UI

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -2,17 +2,29 @@ import router from 'next/router'
 import React from 'react'
 
 function Landing() {
-  return (
-    <div className="h-screen flex flex-col items-center justify-center bg-base text-black">
-      <h1 className="text-4xl font-bold mb-6">Welcome to Persona Builder</h1>
-      <button
-        onClick={() => router.push("/form")}
-        className="bg-accent text-black px-6 py-3 rounded-xl font-semibold hover:bg-black hover:text-white transition-all"
+    return (
+      <div
+        className="h-screen bg-base text-black"
+        style={{
+          backgroundImage: "url('/robot.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
       >
-        Get Started
-      </button>
-    </div>
-  )
-}
+        <div className="mt-40 flex flex-col items-center justify-center">
 
-export default Landing
+            <h1 className="text-4xl font-bold mb-6">Welcome to Persona Builder</h1>
+            <button
+            onClick={() => router.push("/form")}
+            className="bg-accent text-black px-6 py-3 rounded-xl font-semibold hover:bg-black hover:text-white transition-all"
+            >
+            Get Started
+            </button>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Landing;
+  
